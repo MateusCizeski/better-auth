1373f32d18c2c7490b020fc4caaa933df670b638
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-07-06 11:08:50 -0300

    ajuste do gitignore

--- UserApi/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ UserApi/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bf2ddb41a72606143adf8c2f37d1683956fe44e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24894768148d9c5f8c098f59d51b6c4ffc38846d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
