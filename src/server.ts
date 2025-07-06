24894768148d9c5f8c098f59d51b6c4ffc38846d
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-07-06 10:54:47 -0300

    Iniciando refatoração da api utilizando boas práticas de arquitetura

--- src/server.ts
+++
@@ -1,7 +0,0 @@
-import { app } from "./app";
-import { env } from "./config/env";
-import { logger } from "./utils/logger";
-
-app.listen(env.PORT, () => {
-    logger.info(`server running on http://localhost:${env.PORT}`);
-});
