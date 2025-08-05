41bb5d79604da832a265eb9c57b8b319016b38d6
Mateus Cizeski Kogl <mateus.kogl@useall.com.br>
2025-08-05 08:49:54 -0300

    ajuste de referencia entre class libary

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+209a810167a333278eb4d739946d17394fb3fa58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad2ac1c86473ec9def932065e78e158429b508d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
