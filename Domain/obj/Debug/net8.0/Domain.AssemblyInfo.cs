6c827e90c3ace5dc5016c99a34c69791c5716a0b
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-08-07 22:31:27 -0300

    ajuste

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad2ac1c86473ec9def932065e78e158429b508d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f9fc867abaf1d6021aff4477c9698705d0f8b42")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
