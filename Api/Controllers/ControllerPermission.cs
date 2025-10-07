d0917fd09b6c5a696e6c7314e6578c84a7cc2d09
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-10-07 20:13:23 -0300

    permissions http methods

--- Api/Controllers/ControllerPermission.cs
+++ Api/Controllers/ControllerPermission.cs
@@ -14,5 +14,50 @@ namespace Api.Controllers
         {
             _aplicPermission = application;
         }
+
+        [HttpPost]
+        public IActionResult NewPermission([FromBody] NewPermissionDTO dto)
+        {
+            try
+            {
+                var permission = _aplicPermission.NewPermission(dto);
+
+                return RespondSuccess("Permission created with a success.", content: permission);
+            }
+            catch (Exception e) 
+            {
+                return RespondError(e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdatePermission([FromRoute] Guid id, [FromBody] UpdatePermissionDTO dto)
+        {
+            try
+            {
+                var permission = _aplicPermission.UpdatePermission(id, dto);
+
+                return RespondSuccess("Permission updated with a success.", content: permission);
+            }
+            catch (Exception e)
+            {
+                return RespondError(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult RemovePermission([FromRoute] Guid id)
+        {
+            try
+            {
+                _aplicPermission.RemovePermission(id);
+
+                return RespondSuccess("Permission created with a success.");
+            }
+            catch (Exception e)
+            {
+                return RespondError(e.Message);
+            }
+        }
     }
 }
