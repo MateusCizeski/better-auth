49796922fd83b9cf0051557d0d77bc1660334ee4
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-10-09 20:46:39 -0300

    rolepermission and userrole controllers

---
+++ Api/Controllers/ControllerUserRole.cs
@@ -0,0 +1,18 @@
+﻿using ApiBase.Controller.BaseGuid;
+using Application.UserRoles;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/UserRole")]
+    public class ControllerUserRole : GuidController<IAplicUserRole, UserRoleView>
+    {
+        private readonly IAplicUserRole _aplicUserRole;
+        protected ControllerUserRole(IAplicUserRole application) : base(application)
+        {
+            _aplicUserRole = application;
+        }
+    }
+}
