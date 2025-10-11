f7b68b2b37fea5adb9cc1cd1249a9168840489b2
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-10-11 09:57:36 -0300

    Methods layer userRole

--- Api/Controllers/UserRoleController.cs
+++ Api/Controllers/UserRoleController.cs
@@ -14,5 +14,37 @@ namespace Api.Controllers
         {
             _aplicUserRole = application;
         }
+
+        [HttpPost]
+        [Route("{userId}/{roleId}")]
+        public IActionResult BindUserRole([FromRoute] Guid userId, [FromRoute] Guid roleId)
+        {
+            try
+            {
+                _aplicUserRole.BindUserRole(userId, roleId);
+
+                return RespondSuccess("Binding user and role created with success.");
+            }
+            catch (Exception ex) 
+            {
+                return RespondError(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult RemoveBindUserRole([FromRoute] Guid id)
+        {
+            try
+            {
+                _aplicUserRole.RemoveBindUserRole(id);
+
+                return RespondSuccess("Binding user and role removed with success.");
+            }
+            catch (Exception ex)
+            {
+                return RespondError(ex.Message);
+            }
+        }
     }
 }
