dc044ccd10e7da2c6d7d2121bb7ac6e16c261d73
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-10-18 08:18:29 -0300

    Fix for swagger extension

---
+++ Api/Extension/SwaggerExtension.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Api.Extension
+{
+    public static class SwaggerExtension
+    {
+        public static IServiceCollection AddApiDoc(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "API Base",
+                    Version = "v1",
+                    Description = "API com autenticação JWT"
+                });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Insira o token JWT: Bearer {seu token}",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        },
+                        Scheme = "oauth2",
+                        Name = "Bearer",
+                        In = ParameterLocation.Header
+                    },
+                    new List<string>()
+                }
+            });
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseApiDoc(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                c.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
+            });
+
+            return app;
+        }
+    }
+}
